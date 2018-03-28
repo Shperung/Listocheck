a5d038fafac2a95cc63b7b37c7fb691bd3df00d6
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2018-03-28 13:35:25 +0300

    setState

--- src/article/article-class.jsx
+++ src/article/article-class.jsx
@@ -2,17 +2,45 @@
 import React, {Component} from 'react';
 
 export default class ArticleClass extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    }
+  }
+
+ toggleOpen = () => {
+  this.setState({
+      isOpen: !this.state.isOpen
+    }
+  )
+ }
+
   render() {
+    console.log(this.state);
     const {
       id,
       title,
       text
     } = this.props;
+
+    const {
+      isOpen
+    } = this.state;
+
+    const body = isOpen ? <p>{text}</p> : null;
+
     return (
       <article key={id}>
         <h3>{title}</h3>
-        <p>{text}</p>
+        <button onClick= {this.toggleOpen} type="button">
+          {isOpen ? 'close' : 'open'}
+        </button>
+        {body}
       </article>
     )
   }
+
+
 }/*class*/
