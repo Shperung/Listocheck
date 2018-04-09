884018db0b499e49a054584c4af0a56ad7502985
Viktor Krav4uk Home <viktorkrav4uk87@gmail.com>
2018-04-09 10:05:18 +0300

    add data file json ## To make a commit, type your commit message and press CTRL-ENTER. To cancel ## the commit, close the window. To sign off on the commit, press CTRL-S. ## ## You may also reference or close a GitHub issue with this commit.  To do so, ## type `#` followed by the `tab` key.  You will be shown a list of issues ## related to the current repo.  You may also type `owner/repo#` plus the `tab` ## key to reference an issue in a different GitHub repo.
     npm-debug.log                | 36 ++++++++++++++++
     src/article/article-list.jsx |  1 +
     src/data.js                  | 98 ++++++++++++++++++++++++++++++++++++++++++++
     3 files changed, 135 insertions(+)

---
+++ src/data.js
@@ -0,0 +1,98 @@
+export const articles = [
+	{
+		"id": "454563434",
+		"title": "Title1",
+		"text": "text1",
+		"comments": [
+			{
+				"id": "4545634326",
+				"user": "User1-1",
+				"text": "comment1-1",
+			},
+			{
+				"id": "4545634327",
+				"user": "User1-2",
+				"text": "comment1-2",
+			},
+			{
+				"id": "4545634328",
+				"user": "User1-3",
+				"text": "comment1-3",
+			},
+			{
+				"id": "4545634329",
+				"user": "User1-4",
+				"text": "comment1-4",
+			},
+			{
+				"id": "4545631327",
+				"user": "User1-5",
+				"text": "comment1-5",
+			}
+		]
+	},
+	{
+		"id": "4545d3434",
+		"title": "Title2",
+		"text": "text2",
+		"comments": [
+			{
+				"id": "45456dc326",
+				"user": "User2-1",
+				"text": "comment2-1",
+			},
+			{
+				"id": "454563zxc327",
+				"user": "User2-2",
+				"text": "comment2-2",
+			},
+			{
+				"id": "45456zxv4328",
+				"user": "User2-3",
+				"text": "comment2-3",
+			},
+			{
+				"id": "454zxv4329",
+				"user": "User2-4",
+				"text": "comment2-4",
+			},
+			{
+				"id": "45456e1327",
+				"user": "User2-5",
+				"text": "comment2-5",
+			}
+		]
+	},
+	{
+		"id": "4545sdv434",
+		"title": "Title3",
+		"text": "text3",
+		"comments": [
+			{
+				"id": "45456bvn26",
+				"user": "User3-1",
+				"text": "comment3-1",
+			},
+			{
+				"id": "4545zdfgh327",
+				"user": "User3-2",
+				"text": "comment3-2",
+			},
+			{
+				"id": "4545bzbbbb34328",
+				"user": "User3-3",
+				"text": "comment3-3",
+			},
+			{
+				"id": "4545lllll329",
+				"user": "User3-4",
+				"text": "comment3-4",
+			},
+			{
+				"id": "45456fff327",
+				"user": "User3-5",
+				"text": "comment3-5",
+			}
+		]
+	},
+];
