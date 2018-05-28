6075c94aeb675968ff697338ca63ed1e50dc1a05
Viktor Krav4uk Home <viktorkrav4uk87@gmail.com>
2018-05-28 12:13:23 +0300

    onchange input

--- src/form/input.jsx
+++ src/form/input.jsx
@@ -3,9 +3,34 @@ import React, {Component} from 'react';
 
 class Input extends Component {
 
+	state={
+		username: 'test',
+	};
+
+	handleUserChange = (ev) => {
+		{/* Меняю цвет */}
+		if (ev.target.value.length > 10 ) {
+			ev.target.style = 'color: green'
+		} else {
+			ev.target.style = 'color: inherit'
+		}
+		this.setState({
+				username: ev.target.value,
+		});
+
+	};
+
 	render() {
 		return (
-			<input type="text"/>
+			<label htmlFor="username">
+			User: &nbsp;
+				<input
+					id="username"
+					type="text"
+					value={this.state.username}
+					onChange={this.handleUserChange}
+				/>
+			</label>
 		)
 	}
 
